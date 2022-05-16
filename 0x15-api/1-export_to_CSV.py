7ec4152fd45b332b1b7de58c1bdf9daa6dfaffa2
ozswar94 <3630@holbertonschool.com>
2022-05-16 14:41:51 +0000

    test csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,15 +6,15 @@ import sys
 
 
 if __name__ == '__main__':
-    id = str(sys.argv[1])
+    id = sys.argv[1]
     u_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
     t_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
 
     user = requests.get(u_url).json()
     todo = requests.get(t_url).json()
 
-    with open("{}.csv".format(user.get('id')), 'w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("{}.csv".format(id), 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for t in todo:
-            line = [user.get('id'), user.get('name'), t.get('completed'), t.get('title')]
+            line = [id, user.get('name'), t.get('completed'), t.get('title')]
             writer.writerow(line)
