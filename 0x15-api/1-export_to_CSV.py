e6e73842a4ab161960669b15d0f8d601efc19f60
Ozswar <jordan6heat@hotmail.fr>
2022-05-16 14:07:51 +0000

    fix csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ export to csv """
+import csv
 import requests
 import sys
 
@@ -13,10 +14,12 @@ if __name__ == '__main__':
     todo = requests.get(t_url).json()
 
     with open("{}.csv".format(user.get('id')), 'w') as f:
-        for t in todo:
-            line = '"{}","{}","{}","{}"\n'.format(
+        writer = csv.writer(f)
+		  for t in todo:
+            line = [
                 user.get('id'),
                 user.get('name'),
                 t.get('completed'),
                 t.get('title'))
-            f.write(line)
+					 ]
+            writer.writerow(line)
