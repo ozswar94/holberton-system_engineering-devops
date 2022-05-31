e9c1c61c0d4d242f7156d3bac0634be27ab08102
ozswar94 <3630@holbertonschool.com>
2022-05-31 11:16:02 +0000

    add task 0 and 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" handle reddit api """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    this function return the number of subcribers by subreddit
+    """
+    header = {'user-agent': 'APP-Subreddits'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=header)
+    if response.status_code > 300:
+        return None
+    return response.json().get("data").get('subscribers')
