531cbc184ea07d48ae31fd0134099a5eb6749ae0
Ozswar <jordan6heat@hotmail.fr>
2022-06-08 13:39:16 +0000

    add task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+#fix error nginx server
+
+exec { 'fix error':
+    path    => '/usr/local/bin/:/bin/',
+    command => 'sed -i "s/worker_processes 4;/worker_processes 8;/g" /etc/nginx/nginx.conf'
+}
