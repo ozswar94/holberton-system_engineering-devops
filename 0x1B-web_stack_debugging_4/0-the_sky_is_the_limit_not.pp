454e69c63c5ebc5e72e814051ae3e8184e23bb51
ozswar94 <3630@holbertonschool.com>
2022-06-09 09:36:35 +0200

    Update 0-the_sky_is_the_limit_not.pp
    restart server nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 
 exec { 'fix error':
     path    => '/usr/local/bin/:/bin/',
-    command => 'sed -i "s/worker_processes 4;/worker_processes 8;/g" /etc/nginx/nginx.conf'
+    command => 'sed -i "s/worker_processes 4;/worker_processes 8;/g" /etc/nginx/nginx.conf; sudo service nginx restart'
 }
